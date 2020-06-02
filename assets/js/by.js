5171165b3e86aed26a348d25b2ef16f5e3e3aa2a
haryobayu <haryobayu.info@gmail.com>
2020-06-02 20:40:50 +0700

    Setup
    setup awal

---
+++ assets/js/by.js
@@ -0,0 +1,14 @@
+// JavaScript Document
+/* Made by Haryo Bayu 05/12/2019 */
+
+$(document).ready(function(){
+  $(".advscr").hide();
+  $(".zc333").click(function(){
+    $(".advscr").toggle();
+  });
+  
+  $(".advscr2").hide();
+  $(".zc3332").click(function(){
+    $(".advscr2").toggle();
+  });
+});
